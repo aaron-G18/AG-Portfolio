c99fba7c7daddda945f6112edc9d7c99a9dd09db
Aaron Garner <aaron.garner.1982@gmail.com>
2020-08-16 16:54:53 -0700

    added react-icon library and put some icons on the page, played with rotating them.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import BrandStatement from "./components/brandStatement/brandStatement";
 import MyName from "./components/myName/myName";
-
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
+import { AiOutlineCaretLeft, AiOutlineCaretRight } from 'react-icons/ai';
+import { DiJsBadge, DiHtml5, DiReact, DiCss3, DiMongodb, DiJqueryLogo, DiMysql} from 'react-icons/di';
 
 
 class App extends Component {
@@ -23,6 +25,17 @@ class App extends Component {
     return(
     <div className="App">
       <header className="App-header">
+        <AiOutlineArrowLeft className="inline-block" />
+        <AiOutlineArrowRight className="inline-block" />
+        <AiOutlineCaretLeft className="inline-block" />
+        <AiOutlineCaretRight className="inline-block" />
+        <DiJsBadge className="inline-block App-logo" />
+        <DiJqueryLogo className="inline-block App-logo" />
+        <DiHtml5 className="inline-block App-logo" />
+        <DiCss3 className="inline-block App-logo" />
+        <DiReact className="inline-block App-logo" />
+        <DiMongodb className="inline-block App-logo" />
+        <DiMysql className="inline-block App-logo" />
         <h1><MyName /></h1>
         <img src={logo} className="App-logo" alt="logo" />
         <br />
--- src/components/brandStatement/brandStatement.js
+++ src/components/brandStatement/brandStatement.js
@@ -5,12 +5,14 @@ export default function brandStatement(props) {
   // functions
 
   return (
-    <Wave 
-    text="Full Stack Web Developer with experience programming building automation systems and creating graphical user interfaces for those systems that recently earned a certificate of completion from UC Berkeley Extension for their Full-Stack Coding Bootcamp. Skills include JavaScript, JQuery, HTML, CSS, MySQL, MongoDB, Express, React, and Node (MERN stack). Extensive customer service and project management experience gives me different perspectives when working toward solutions to problems. Strengths include strong attention to detail, works great individually or collaboratively with teammates and end-users. Adept problem-solver and quick learner, eager to join a team and share my experience and skills to bring my perspective and talents to the table. "
-    effect="verticalFadeIn"
-    effectChange={1.0}
-    effectDuration={0.001}
-    iterations={1}
-    />
+    <div className="brand-statement-wrapper">
+      <Wave 
+      text="Full Stack Web Developer with experience programming building automation systems and creating graphical user interfaces for those systems that recently earned a certificate of completion from UC Berkeley Extension for their Full-Stack Coding Bootcamp. Skills include JavaScript, JQuery, HTML, CSS, MySQL, MongoDB, Express, React, and Node (MERN stack). Extensive customer service and project management experience gives me different perspectives when working toward solutions to problems. Strengths include strong attention to detail, works great individually or collaboratively with teammates and end-users. Adept problem-solver and quick learner, eager to join a team and share my experience and skills to bring my perspective and talents to the table. "
+      effect="verticalFadeIn"
+      effectChange={1.0}
+      effectDuration={0.001}
+      iterations={1}
+      />
+    </div>
   );
 }
