a7cb7534b224f16ef2b8fa1855176a56e0789e54
Aaron Garner <aaron.garner.1982@gmail.com>
2020-08-16 15:00:08 -0700

    removed react-pose library (depricated). Installed react-animated-text library and put a couple of test chunks of text on the page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import posed from 'react-pose';
+import { Wave } from 'react-animated-text';
 
+const MyName = () => (
+  <Wave 
+  text="Aaron Garner"
+  effect="verticalFadeIn"
+  effectChange={1.0}
+  delay={3.0}
+  // iterations={1}
+  />
+);
 
-let MyName = posed.div({
-  visible: { opacity: 1 },
-  hidden: { opacity: 0 }
-});
+const BrandStatement = () => (
+  <Wave 
+  text="Full Stack Web Developer with experience programming building automation systems and creating graphical user interfaces for those systems that recently earned a certificate of completion from UC Berkeley Extension for their Full-Stack Coding Bootcamp. Skills include JavaScript, JQuery, HTML, CSS, MySQL, MongoDB, Express, React, and Node (MERN stack). Extensive customer service and project management experience gives me different perspectives when working toward solutions to problems. Strengths include strong attention to detail, works great individually or collaboratively with teammates and end-users. Adept problem-solver and quick learner, eager to join a team and share my experience and skills to bring my perspective and talents to the table. "
+  effect="verticalFadeIn"
+  effectChange={1.0}
+  effectDuration={0.001}
+  iterations={1}
+  />
+);
+
 
 class App extends React.Component {
   constructor (props) {
@@ -18,24 +33,18 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    setInterval(() => {
-      this.setState({ isVisible: !this.state.isVisible });
-    }, 1000);
+   
   }
-
-
 
 
   render() {
     return(
     <div className="App">
       <header className="App-header">
-        <MyName 
-          className="box" 
-          pose={this.state.isVisible ? "visible" : "hidden"}
-          >Aaron Garner 
-        </MyName>
+        <MyName />
         <img src={logo} className="App-logo" alt="logo" />
+        <br />
+        <BrandStatement />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
