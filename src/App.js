c3f335bd44341da94311a3a5e2427d1f5bc5d9f3
Aaron Garner <aaron.garner.1982@gmail.com>
2020-08-16 13:59:17 -0700

    Tested out react-pose animation on my name at top of page (fade in and out on interval)... worked well.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import posed from 'react-pose';
 
-function App() {
-  return (
+
+let MyName = posed.div({
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 }
+});
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      isVisible: true,
+    };
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ isVisible: !this.state.isVisible });
+    }, 1000);
+  }
+
+
+
+
+  render() {
+    return(
     <div className="App">
       <header className="App-header">
-        <h1>Aaron Garner</h1>
+        <MyName 
+          className="box" 
+          pose={this.state.isVisible ? "visible" : "hidden"}
+          >Aaron Garner 
+        </MyName>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,7 +49,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  };
 }
 
 export default App;
