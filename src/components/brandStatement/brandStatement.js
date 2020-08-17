75a4e9725a8c6863663c223518bd4cc609e52e8e
Aaron Garner <aaron.garner.1982@gmail.com>
2020-08-16 18:02:22 -0700

    Trying out different approaches to animating the brand statement without splitting words on the line breaks.

--- src/components/brandStatement/brandStatement.js
+++ src/components/brandStatement/brandStatement.js
@@ -4,10 +4,20 @@ import { Wave } from 'react-animated-text';
 export default function brandStatement(props) {
   // functions
 
+  
+
   return (
     <div className="brand-statement-wrapper">
       <Wave 
-      text="Full Stack Web Developer with experience programming building automation systems and creating graphical user interfaces for those systems that recently earned a certificate of completion from UC Berkeley Extension for their Full-Stack Coding Bootcamp. Skills include JavaScript, JQuery, HTML, CSS, MySQL, MongoDB, Express, React, and Node (MERN stack). Extensive customer service and project management experience gives me different perspectives when working toward solutions to problems. Strengths include strong attention to detail, works great individually or collaboratively with teammates and end-users. Adept problem-solver and quick learner, eager to join a team and share my experience and skills to bring my perspective and talents to the table. "
+      text="Full Stack Web Developer with experience programming building automation systems and creating graphical user interfaces "
+      effect="verticalFadeIn"
+      effectChange={0}
+      effectDuration={.5}
+      speed={200}
+      iterations={1}
+      />
+      <Wave 
+      text="for those systems that recently earned a certificate of completion from UC Berkeley Extension for their Full-Stack "
       effect="verticalFadeIn"
       effectChange={0}
       effectDuration={.5}
@@ -17,3 +27,9 @@ export default function brandStatement(props) {
     </div>
   );
 }
+
+
+// 
+
+
+//    Coding Bootcamp. Skills include JavaScript, JQuery, HTML, CSS, MySQL, MongoDB, Express, React, and Node (MERN stack). Extensive customer service and project management experience gives me different perspectives when working toward solutions to problems. Strengths include strong attention to detail, works great individually or collaboratively with teammates and end-users. Adept problem-solver and quick learner, eager to join a team and share my experience and skills to bring my perspective and talents to the table.
