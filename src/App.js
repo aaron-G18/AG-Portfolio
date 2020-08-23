020d5bae6a666dda0f4b9024ba58e6679e84a782
Aaron Garner <aaron.garner.1982@gmail.com>
2020-08-23 14:36:06 -0700

    Added notes to app.js for possible things to add.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import './App.css';
 import BrandStatement from "./components/brandStatement/brandStatement";
 import MyName from "./components/myName/myName";
-import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
-import { AiOutlineCaretLeft, AiOutlineCaretRight } from 'react-icons/ai';
-import { DiJsBadge, DiHtml5, DiReact, DiCss3, DiMongodb, DiJqueryLogo, DiMysql} from 'react-icons/di';
+import {
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight
+} from 'react-icons/ai';
+import {
+  AiOutlineCaretLeft,
+  AiOutlineCaretRight
+} from 'react-icons/ai';
+import {
+  DiJsBadge,
+  DiHtml5,
+  DiReact,
+  DiCss3,
+  DiMongodb,
+  DiJqueryLogo,
+  DiMysql
+} from 'react-icons/di';
 import BottomLinks from "./components/bottomLinks/bottomLinks";
 import FrontPage from "./components/frontPage/frontPage";
 
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       isVisible: true,
@@ -18,40 +34,60 @@ class App extends Component {
   };
 
   componentDidMount() {
-   
+
   }
 
 
   render() {
-    return(
-    <div className="App">
-      <header className="App-header">
-        {/* <AiOutlineArrowLeft className="inline-block" size="3em" />
-        <AiOutlineArrowRight className="inline-block" size="3em" />
-        <AiOutlineCaretLeft className="inline-block" size="3em" />
-        <AiOutlineCaretRight className="inline-block" size="3em" />
-        
-        <h1><MyName /></h1>
-        <hr />
-        <BrandStatement /> */}
-        <FrontPage />
-        <div>
-        <DiJsBadge className="inline-block App-logo" size="3em" />
-        <DiJqueryLogo className="inline-block App-logo" size="3em" />
-        <DiHtml5 className="inline-block App-logo" size="3em" />
-        <DiCss3 className="inline-block App-logo" size="3em" />
-        <DiReact className="inline-block App-logo" size="3em" />
-        <DiMongodb className="inline-block App-logo" size="3em" />
-        <DiMysql className="inline-block App-logo" size="3em" />
-        <p>
-          links for pages will go here.
-        </p>
-        <span>test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test </span>
-        </div>
-        <BottomLinks />
-        
-      </header>
-    </div>
+    return ( <
+      div className = "App" >
+      <
+      header className = "App-header" > {
+        /* <AiOutlineArrowLeft className="inline-block" size="3em" />
+                <AiOutlineArrowRight className="inline-block" size="3em" />
+                <AiOutlineCaretLeft className="inline-block" size="3em" />
+                <AiOutlineCaretRight className="inline-block" size="3em" />
+                
+                <h1><MyName /></h1>
+                <hr />
+                <BrandStatement /> */
+      } <
+      FrontPage / >
+      <
+      div >
+      <
+      DiJsBadge className = "inline-block App-logo"
+      size = "3em" / >
+      <
+      DiJqueryLogo className = "inline-block App-logo"
+      size = "3em" / >
+      <
+      DiHtml5 className = "inline-block App-logo"
+      size = "3em" / >
+      <
+      DiCss3 className = "inline-block App-logo"
+      size = "3em" / >
+      <
+      DiReact className = "inline-block App-logo"
+      size = "3em" / >
+      <
+      DiMongodb className = "inline-block App-logo"
+      size = "3em" / >
+      <
+      DiMysql className = "inline-block App-logo"
+      size = "3em" / > {
+        /* NODE.js */ } <
+      p >
+      links
+      for pages will go here. <
+      /p> <
+      span > test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test < /span> <
+      /div> <
+      BottomLinks / >
+
+      <
+      /header> <
+      /div>
     );
   };
 }
