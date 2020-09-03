e736f2633beb62d9ff78c630839bb9f50c2757ca
Aaron Garner <aaron.garner.1982@gmail.com>
2020-09-02 17:00:18 -0700

    updated names for files for animated name and cleaned up line breaks in app.js. Also playing with styling for animated name to be float left an dbe a 'sticky' side bar element.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import { AiOutlineCaretLeft, AiOutlineCaretRight } from 'react-icons/ai';
 import { DiJsBadge, DiHtml5, DiReact, DiCss3, DiMongodb, DiJqueryLogo, DiMysql, DiNodejsSmall } from 'react-icons/di';
 import BottomLinks from "./components/bottomLinks/bottomLinks";
-import FrontPage from "./components/frontPage/frontPage";
+import AnimatedName from "./components/animatedName/animatedName";
 import Project from "./components/projects/projects";
 import ReactTooltip from 'react-tooltip';
 
@@ -40,47 +40,27 @@ class App extends Component {
         <h1><MyName /></h1>
         <hr />
         <BrandStatement /> */}
-        <FrontPage />
+        <AnimatedName />
         <div>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <Project />
-          <div data-tip="JavaScript" className="inline-block">
-            <DiJsBadge data-tip="JavaScript" className="inline-block App-logo" size="3em" />
-          </div>
+            <Project />
+            <div data-tip="JavaScript" className="inline-block">
+              <DiJsBadge data-tip="JavaScript" className="inline-block App-logo" size="3em" />
+            </div>
           
-          <div className="inline-block">
-            <DiJqueryLogo className="inline-block App-logo" size="3em" />
+            <div className="inline-block">
+              <DiJqueryLogo className="inline-block App-logo" size="3em" />
+            </div>
+          <DiHtml5 className="inline-block App-logo" size="3em" />
+          <DiCss3 className="inline-block App-logo" size="3em" />
+          <DiReact className="inline-block App-logo" size="3em" />
+          <DiMongodb className="inline-block App-logo" size="3em" />
+          <DiMysql className="inline-block App-logo" size="3em" />
+          <div data-tip="NodeJS">
+          <DiNodejsSmall className="inline-block App-logo" size="3em" />
           </div>
-        <DiHtml5 className="inline-block App-logo" size="3em" />
-        <DiCss3 className="inline-block App-logo" size="3em" />
-        <DiReact className="inline-block App-logo" size="3em" />
-        <DiMongodb className="inline-block App-logo" size="3em" />
-        <DiMysql className="inline-block App-logo" size="3em" />
-        <div data-tip="NodeJS">
-        <DiNodejsSmall className="inline-block App-logo" size="3em" />
-        </div>
-        {/* NODE.js */}
-        <p>
-          links for pages will go here.
-        </p>
+          <p>
+            links for pages will go here.
+          </p>
         </div>
         <BottomLinks />
         <ReactTooltip place="top" type="dark" effect="float"/>
