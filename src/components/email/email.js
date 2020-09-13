d694426e56adae615997744eeca8beb232dabb5f
Aaron Garner <aaron.garner.1982@gmail.com>
2020-09-13 12:57:27 -0700

    added suit image.

---
+++ src/components/email/email.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+
+
+export default function email(props) {
+  // functions
+
+  return (
+    <div className="email">
+        Email!
+    </div>
+  );
+}
