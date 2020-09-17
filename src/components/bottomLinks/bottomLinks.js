4f960019a221be049ecc0afa7e2f7a668390b7b9
Aaron Garner <aaron.garner.1982@gmail.com>
2020-09-17 16:22:43 -0700

    corrected display (inline) of bottom links.

--- src/components/bottomLinks/bottomLinks.js
+++ src/components/bottomLinks/bottomLinks.js
@@ -9,22 +9,22 @@ export default function bottomLinks(props) {
 
   return (
     <div className="bottom-links">
-      <div data-tip="My LinkedIn" className="link">
+      <div data-tip="My LinkedIn" className="bottom-link">
         <a href="https://www.linkedin.com/in/aaron-garner-53066346/" target="_blank" rel="noopener noreferrer">
           <AiFillLinkedin className="linkedin" size="3vw" color="blue" />
         </a>
       </div>
-      <div data-tip="My Github" className="link">
+      <div data-tip="My Github" className="bottom-link">
         <a href="https://github.com/aaron-G18" target="_blank" rel="noopener noreferrer">
           <AiFillGithub className="github" size="3vw" color="blue" />
         </a>
       </div>
-      <div data-tip="My Resume" className="link">
+      <div data-tip="My Resume" className="bottom-link">
         <a href="./resume/resume.pdf" target="_blank" rel="noopener noreferrer">
           <AiOutlineFileText className="email" size="3vw" color="blue" />
         </a>
       </div>
-      <div data-tip="Email me" className="link">
+      <div data-tip="Email me" className="bottom-link">
         <a href="mailto:aaron.garner.1982@gmail.com" target="_blank" rel="noopener noreferrer">
           <AiOutlineMail className="email" size="3vw" color="blue" />
         </a>
