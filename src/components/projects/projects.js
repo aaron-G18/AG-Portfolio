d03e8a9b4b31a3ddb566fc8f41f595df6e5f19ee
Aaron Garner <aaron.garner.1982@gmail.com>
2020-09-21 16:43:39 -0700

    Names of projects as place holders for which projects to put where.

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -10,8 +10,8 @@ export default function bottomLinks(props) {
     <div className="projects">
         <div className="project1">
           <div className="proj1-text">
-            <h3>Project 1 Title</h3>
-            <p>The description of the project will go here. Not too much text but enough to explain the project and some of the tools/technologies used and my contributions to them.</p>
+            <h3>Resource Center</h3>
+            <p>The description of the project will go here. Not too much text but enough to explain the project and some of the tools/technologies used and my contributions to them. DESCRIPTION OF RESOURCE CENTER</p>
           </div>
           <img className="proj1-img" src="https://dummyimage.com/300x200/000000/0011ff&text=Place+Holder" alt="screen shot of <project name>"></img>
         </div>
@@ -19,8 +19,8 @@ export default function bottomLinks(props) {
         <hr className="hr-short" />
         <div className="project2">
           <div className="proj2-text">
-            <h3>Project 2 Title</h3>
-            <p>The description of the project will go here. Not too much text but enough to explain the project and some of the tools/technologies used and my contributions to them.</p>
+            <h3>Community First</h3>
+            <p>The description of the project will go here. Not too much text but enough to explain the project and some of the tools/technologies used and my contributions to them. DESCRIPTION OF COMMUNITY FIRST</p>
           </div>
           <img className="proj2-img" src="https://dummyimage.com/300x200/000000/0011ff&text=Place+Holder" alt="screen shot of <project name>"></img>
         </div>
@@ -28,8 +28,8 @@ export default function bottomLinks(props) {
         <hr className="hr-short" />
         <div className="project3">
           <div className="proj3-text">
-            <h3>Project 3 Title</h3>
-            <p>The description of the project will go here. Not too much text but enough to explain the project and some of the tools/technologies used and my contributions to them.</p>
+            <h3>Google Book Search</h3>
+            <p>The description of the project will go here. Not too much text but enough to explain the project and some of the tools/technologies used and my contributions to them. GOOGLE BOOK SEARCH</p>
           </div>
           <img className="proj3-img" src="https://dummyimage.com/300x200/000000/0011ff&text=Place+Holder" alt="screen shot of <project name>"></img>
         </div>
