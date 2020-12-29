59283cbec1dd396d1aeb819bb7e0b784411ca428
Aaron Garner <aaron.garner.1982@gmail.com>
2020-12-28 16:14:25 -0800

    updated sidebar to correct class (should have been className) attributes.

--- src/components/sideBar/sideBar.js
+++ src/components/sideBar/sideBar.js
@@ -7,14 +7,14 @@ export default function frontPage(props) {
 
   return (
     <div className="sidebar">
-        <div class="animated-title">
-            <div class="text-top">
+        <div className="animated-title">
+            <div className="text-top">
                 <div className="top-box">
                 <span>Aaron Garner</span>
                     <img className="aaron-suit" src="./images/Aaron_Suit_Cropped.jpg" alt="aaron in suit" />
                 </div>
             </div>
-            <div class="text-bottom">
+            <div className="text-bottom">
                 <div className="bottom-content">
                     <SideLinks />
                 </div>
