68ff40e938d04ac4dd37eec4d8909c79db577308
Aaron Garner <aaron.garner.1982@gmail.com>
2020-12-29 10:50:51 -0800

    Removed data-tips from navbar for mobile.

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,22 +6,22 @@ export default function navbar(props) {
   return (
     <div className="navbar">
       <h1>Aaron Garner</h1>
-      <div data-tip="My LinkedIn" className="bottom-link">
+      <div className="bottom-link">
       <a href="https://www.linkedin.com/in/aaron-garner-53066346/" target="_blank" rel="noopener noreferrer">
         <AiFillLinkedin className="linkedin" size="5vw" color="blue" />
       </a>
     </div>
-    <div data-tip="My Github" className="bottom-link">
+    <div className="bottom-link">
       <a href="https://github.com/aaron-G18" target="_blank" rel="noopener noreferrer">
           <AiFillGithub className="github" size="5vw" color="blue" />
       </a>
     </div>
-    <div data-tip="My Resume" className="bottom-link">
+    <div className="bottom-link">
       <a href="./resume/resume.pdf" target="_blank" rel="noopener noreferrer">
           <AiOutlineFileText className="email" size="5vw" color="blue" />
       </a>
     </div>
-    <div data-tip="Email me" className="bottom-link">
+    <div className="bottom-link">
       <a href="mailto:aaron.garner.1982@gmail.com" target="_blank" rel="noopener noreferrer">
           <AiOutlineMail className="email" size="5vw" color="blue" />
       </a>
